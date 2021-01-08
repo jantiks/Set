3f739e13ada795b761805c1feda014e7ffa5fbee
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-08 12:40:31 +0400

    Initial Commit

---
+++ Set game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set game
+//
+//  Created by Tigran on 1/8/21.
+//  Copyright © 2021 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
